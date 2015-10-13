2f63795406d593e4ecdc1ea0a875002cfdde75a0
Connor Finholt <connor.finholt@gmail.com>
2015-10-12 23:14:23 -0500

    Added a test activity and did some testing adjusting screen brightness, etc.

---
+++ app/src/main/java/com/example/calebpotratz/unlockapp/ConnorTest.java
@@ -0,0 +1,68 @@
+package com.example.calebpotratz.unlockapp;
+
+import android.content.Context;
+import android.os.PowerManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class ConnorTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connor_test);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_connor_test, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void dimDisplay(View view)
+    {
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.screenBrightness = 0;
+        getWindow().setAttributes(params);
+
+        /*PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "My Tag");
+        wl.acquire();
+        new Timer().schedule(
+                new TimerTask() {
+                    @Override
+                    public void run() {
+
+                    }
+                },
+                10000
+        );
+        wl.release();
+        PowerManager.WakeLock wl2 = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "My Tag");
+        wl2.acquire();
+        wl2.release();*/
+    }
+}
